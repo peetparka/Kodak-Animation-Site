853083f895f3bdfb93883c63a954e1a8aa486e00
peetparka <pnan@ucdavis.edu>
2016-03-11 18:51:28 -0800

    version.uno

---
+++ script.js
@@ -0,0 +1,6 @@
+var modaluno = document.getElementById("modaluno");
+var kodak = document.getElementById("kodak");
+function modal() {
+    modaluno.style.visibility = "visible";   
+}
+kodak.onclick = modal;
